6cc864b191431eab2b2a541036301a79d2bd8513
nathakits <nathakit.sae-tan@cuinda.com>
2016-06-07 17:36:06 +0700

    upload p5.js sketch audio viz

---
+++ audio-viz-02/sketch.js
@@ -0,0 +1,54 @@
+var mic, fft;
+var analyzer;
+
+function setup() {
+   createCanvas(640,640);
+   
+   mic = new p5.AudioIn();
+   mic.start();
+   fft = new p5.FFT();
+   fft.setInput(mic);
+  
+   // create a new Amplitude analyzer
+  analyzer = new p5.Amplitude();
+
+  // Patch the input to an volume analyzer
+  analyzer.setInput(mic);
+  
+}
+
+var ellipses = [];
+
+function draw() {
+  //colorMode(RGB,255,255,255,1);
+  background(50);
+     
+  
+   // Get the average (root mean square) amplitude
+  var rms = analyzer.getLevel();
+  // Draw an ellipse with size based on volume
+
+  strokeWeight(2);
+  stroke(255);
+  noFill();
+  ellipses.push({diam:rms*100,timestamp:millis()});
+
+  for (var i = 0; i < ellipses.length; i++){
+    var e = ellipses[i];
+    // if current time - timestamps I was created is < 100
+    // then I am still alive and should be drawn
+    if (millis() - e.timestamp <100) {
+      e.diam += e.diam/((millis()-e.timestamp)); //linear interpolation easing functions
+
+      var d = Math.min(Math.min(width,height),e.diam); // constrain the ellipse within the screen bounds
+      ellipse(width/2, height/2, d, d);      
+    } else {
+      // if I am dead, remove me from system memory
+      ellipses.splice(i,1);
+      
+    }
+  }
+  var c = color(0,0,1,.5);
+  fill(c);
+  rect(0,0,width,height);
+}
